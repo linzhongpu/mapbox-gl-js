2c3a13a34316523d1368c61775ef4826d9672688
linzhongpu <42532869+linzhongpu@users.noreply.github.com>
2018-08-23 23:07:04 +0800

    Translate down 100%
    line 2 to line 6

--- docs/pages/example/mapbox-gl-directions.js
+++ docs/pages/example/mapbox-gl-directions.js
@@ -1,9 +1,9 @@
 /*---
-title: Display driving directions
+title: 显示行驶方向
 description: >-
-  Use the [mapbox-gl-directions](https://github.com/mapbox/mapbox-gl-directions)
-  plugin to show results from the Mapbox Directions API. Click the map to add an
-  origin and destination.
+  使用[mapbox-gl-directions](https://github.com/mapbox/mapbox-gl-directions)
+  插件来显示来自Mapbox Directions API的结果. 点击地图可添加
+  起点与终点。
 tags:
   - controls-and-overlays
 pathname: /mapbox-gl-js/example/mapbox-gl-directions/
