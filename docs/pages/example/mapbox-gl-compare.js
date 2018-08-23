a2b7a4ee8fff5286f7804919074d0b3522ea7da6
linzhongpu <42532869+linzhongpu@users.noreply.github.com>
2018-08-23 23:04:47 +0800

    Translate down 100%
    line 2 and line 4

--- docs/pages/example/mapbox-gl-compare.js
+++ docs/pages/example/mapbox-gl-compare.js
@@ -1,7 +1,7 @@
 /*---
-title: Swipe between maps
+title: 在不同地图之间滑动切换
 description: >-
-  Use [mapbox-gl-compare](https://github.com/mapbox/mapbox-gl-compare) to swipe between and synchronize two maps.
+  使用[mapbox-gl-compare](https://github.com/mapbox/mapbox-gl-compare)在两个地图之间进行滑动切换和同步。
 tags:
   - controls-and-overlays
 pathname: /mapbox-gl-js/example/mapbox-gl-compare/
